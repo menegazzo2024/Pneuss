36bdde6bd3f13f58936ef503b34e8a943866d42c
menegazzo2024 <projetosmenegazzo@gmail.com>
2025-05-02 13:20:02 -0300

    update

--- pages/Resumo.py
+++ pages/Resumo.py
@@ -1,11 +1,17 @@
 import streamlit as st
+import sweetviz as sv
 import pandas as pd
-from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+from streamlit.components.v1 import html
 
-if 'df' in st.session_state:
-    df = st.session_state['df']
-    profile = ProfileReport(df, title="Relatório de Perfil", explorative=True)
-    st_profile_report(profile)
+# carrega DataFrame em st.session_state['df']
+df = st.session_state.get('df')
+if df is None:
+    st.warning("Faça upload na página anterior.")
 else:
-    st.warning("Nenhum DataFrame disponível para análise.")
+    df["PRESSÃO"] = pd.to_numeric(df["PRESSÃO"], errors="coerce")
+    report = sv.analyze(df)
+    report_path = "sweetviz_report.html"
+    report.show_html(report_path, open_browser=False)
+    # injeta o HTML gerado na app
+    with open(report_path, "r", encoding="utf-8") as f:
+        html(f.read(), height=800, scrolling=True)
